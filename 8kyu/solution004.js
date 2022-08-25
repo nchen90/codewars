c508279c79af64ec24921d27e5c2d77d2eaf774f
Nick Chen <nickchen90@gmail.com>
2022-08-24 20:35:03 -0400

    Added codewars for 6-24-2022

---
+++ 8kyu/solution004.js
@@ -0,0 +1,7 @@
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order
+
+//My Solution
+
+function digitize(n) {
+   return Array.from(string(n), Number).reverse()
+}
