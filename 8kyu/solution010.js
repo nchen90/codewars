b950af086b8223404e0c2c0517a041a0e2962fd8
Nick Chen <nickchen90@gmail.com>
2022-08-29 20:27:23 -0400

    Added codewars for 8-29-2022

---
+++ 8kyu/solution010.js
@@ -0,0 +1,6 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// My solution
+function invert(array) {
+  return  array.map(x => x * -1)
+}
