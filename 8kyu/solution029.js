a3baa85893fe24a8107125fd559ff06fb6145b81
Nick Chen <nickchen90@gmail.com>
2022-12-02 17:57:31 -0500

    added codewars for 12-2-2022

---
+++ 8kyu/solution029.js
@@ -0,0 +1,30 @@
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// "I love you"
+// "a little"
+// "a lot"
+// "passionately"
+// "madly"
+// "not at all"
+// If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+
+// My Solution
+
+function howMuchILoveYou(nbPetals) {
+    let arr = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"]
+    
+
+    for(let i = 0; i < arr.length; i++){
+        if(nbPetals % 6 == 0){
+        return arr[5]
+        } else if(nbPetals % 6 == i) {
+        return arr[i - 1]
+        }
+    }
+}
+
+howMuchILoveYou(7)
