d45eb2a1cfd614afb064ddefc4150072cefb8da3
Nick Chen <nickchen90@gmail.com>
2022-12-06 16:11:21 -0500

    added codewars for 12-6-2022

---
+++ 7kyu/solution010.js
@@ -0,0 +1,21 @@
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+// My Solution
+function reverseLetter(str) {
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    return str.split('').filter(x => letters.includes(x)) 
+}
+
+console.log("krishan")
+
