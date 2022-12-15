80a44a64eb9b6a93dba20fc087b2ffbd946de884
Nick Chen <nickchen90@gmail.com>
2022-12-14 21:44:49 -0500

    added codewars for 12-14-22

--- 6kyu/solution001.js
+++ 6kyu/solution001.js
@@ -1,13 +1,52 @@
-// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+// The number 898989 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying
+// "Eureka"? Because this sum gives the same number: 89=81+9289 = 8^1 + 9^289=8 
+// 1
+//  +9 
+// 2
+ 
+// The next number in having this property is 135135135:
+
+// See this property again: 135=11+32+53135 = 1^1 + 3^2 + 5^3135=1 
+// 1
+//  +3 
+// 2
+//  +5 
+// 3
+ 
+
+// Task
+// We need a function to collect these numbers, that may receive two integers aaa, bbb that defines the range [a,b][a, b][a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+// Examples
+// Let's see some cases (input -> output):
+
+// 1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// 1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+// If there are no numbers of this kind in the range [a,b][a, b][a,b] the function should output an empty list.
+
+// 90, 100 --> []
 
 // My Solution 
 
-function sortArray(array) {
-    let sorted = []
-    array.forEach((num, i) => {
-        if(num % 2 !== 0 ){
-            console.log(num)
-        }
-    })
+function sumDigPow(a, b) {
+	let arr = []
+    for(i = a; i <= b; i++){
+        let num = i.toString().split('').map(Number)
+        
+        console.log(Math.pow(num, 1))
+    }
 }
 
+console.log(sumDigPow(1, 100))
+
+function sumDigPow(a, b) {
+    var arr = [];
+    for (var i = a; i <= b; i++) {
+      var sum = 0;
+      for (var j = 0; j <= String(i).length; j++) {
+        sum += Math.pow(parseInt(String(i)[j]), j+1);  
+        if (sum == i) arr.push(i);
+      }
+    }
+    return arr;
+  }
