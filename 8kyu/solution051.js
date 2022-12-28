6d0fb47dfc39dfc42efeccc288849e05f0ffe821
Nick Chen <nickchen90@gmail.com>
2022-12-27 22:03:53 -0500

    added codewars for 12-27-22

---
+++ 8kyu/solution051.js
@@ -0,0 +1,14 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+// My Solution
+function fakeBin(x){
+    return [...x].map(a => {
+    if(a < 5){
+    return 0
+    }else {
+    return 1
+    }
+    }).join('')
+  }
