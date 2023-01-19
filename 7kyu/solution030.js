051b78133086c18e6b4e5776cad0fc7ce522208f
Nick Chen <nickchen90@gmail.com>
2023-01-19 17:11:58 -0500

    added codewars for 1-19-23

---
+++ 7kyu/solution030.js
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/569512b7707bc1b88200002f/train/javascript
+
+// This is a kata series that you can only solve using recursion.
+// ##2 - Fibonacci number
+
+// In mathematical terms, the sequence f(n) of fibonacci numbers is defined by the recurrence relation
+
+// f(n) = f(n-1) + f(n-2)
+
+// with seed values
+
+// f(1) = 1 and f(2) = 1
+
+// #Your task
+
+// You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
+
+// My Solution
+
+const fibonacci = n => {
+    if(n <= 2){
+        return 1
+    }else {
+        fibonacci(n-1) + fibonacci(n-2)
+    }
+};
+
+console.log(fibonacci(2))
