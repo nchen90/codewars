21d924b4313aff1183dbabc9f0a9bf1f95c50f31
Nick Chen <nickchen90@gmail.com>
2023-03-01 14:37:30 -0500

    added codewars for 3-1-23

---
+++ 7kyu/solution039.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a04da9e298a7ee43000111/train/javascript
+
+// Write reverseList function that simply reverses lists.
+
+// My Solution
+function reverseList(arr) {
+    return arr.sort((a, b) => b - a);
+  }
