3c1cae0a3ee4772e424c22439547ec02dbe9e2bb
Nick Chen <nickchen90@gmail.com>
2023-03-08 17:57:27 -0500

    added codewars for 3-8-23

---
+++ 8kyu/solutuion078.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// My Soltuion 
+function printArray(array){
+    return array.join(',')
+  }
