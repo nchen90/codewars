f81dfcc718e3fa822b4a1a539a941ba22486bc6d
Nick Chen <nickchen90@gmail.com>
2023-03-30 17:38:59 -0400

    added codewars for 3-30-23

---
+++ 6kyu/solution30.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5208f99aee097e6552000148/train/javascript
+
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+// My Solution
+function solution(string) {
+let str = string.split('')
+let arr = []
+    for (let i = 0; i < str.length; i++) {
+    	(str[i] !== str[i].toUpperCase()) ? arr.push(str[i]) : arr.push(` ${str[i]}`)  
+    }
+    return arr.join('')
+}
