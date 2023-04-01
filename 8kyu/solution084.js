4623ef48aecd7bf70fdf7dd847522c2e16d60203
Nick Chen <nickchen90@gmail.com>
2023-04-01 18:41:01 -0400

    added codewars for 4-1-23

---
+++ 8kyu/solution084.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// My Solution 
+
+function apple(x){
+    return  (x**2 > 1000) ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+   }
