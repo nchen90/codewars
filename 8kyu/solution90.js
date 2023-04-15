cca8bded842ea7e7efe5d6c0b8a895135a60b57f
Nick Chen <nickchen90@gmail.com>
2023-04-14 23:00:44 -0400

    added codewars for 4-14-23

---
+++ 8kyu/solution90.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/55f73f66d160f1f1db000059/train/javascript
+
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+
+// My Solution
+function combineNames(first, last) {
+    return `${first} ${last}`
+  }                 
